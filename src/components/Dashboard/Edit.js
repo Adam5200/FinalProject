82c79e3d445516dadca229fe31a447c08eead107
Adam Hendrickson <106487414+Adam5200@users.noreply.github.com>
2022-10-01 17:39:36 -0500

    first commit

---
+++ src/components/Dashboard/Edit.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+
+const Edit = ({ employees, selectedEmployee, setEmployees, setIsEditing }) => {
+  const id = selectedEmployee.id;
+
+  const [firstName, setFirstName] = useState(selectedEmployee.firstName);
+  const [lastName, setLastName] = useState(selectedEmployee.lastName);
+  const [email, setEmail] = useState(selectedEmployee.email);
+  const [salary, setSalary] = useState(selectedEmployee.salary);
+  const [date, setDate] = useState(selectedEmployee.date);
+
+  const handleUpdate = e => {
+    e.preventDefault();
+
+    //ensure all fields are filled out
+    if (!firstName || !lastName || !email || !salary || !date) {
+      return Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: 'All fields are required.',
+        showConfirmButton: true,
+      });
+    }
+
+    //create employee object
+    const employee = {
+      id,
+      firstName,
+      lastName,
+      email,
+      salary,
+      date,
+    };
+
+    //place the employee into their designated numeric positions
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].id === id) {
+        employees.splice(i, 1, employee);
+        break;
+      }
+    }
+
+    //save the employees data to local storage
+    localStorage.setItem('employees_data', JSON.stringify(employees));
+    setEmployees(employees);
+    setIsEditing(false);
+
+    //notify user that the information has been updated
+    Swal.fire({
+      icon: 'success',
+      title: 'Updated!',
+      text: `${employee.firstName} ${employee.lastName}'s data has been updated.`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
+  return (
+    <div className="small-container">
+      <form onSubmit={handleUpdate}>
+        <h1>Edit Employee</h1>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor="salary">Salary ($)</label>
+        <input
+          id="salary"
+          type="number"
+          name="salary"
+          value={salary}
+          onChange={e => setSalary(e.target.value)}
+        />
+        <label htmlFor="date">Date</label>
+        <input
+          id="date"
+          type="date"
+          name="date"
+          value={date}
+          onChange={e => setDate(e.target.value)}
+        />
+        <div style={{ marginTop: '30px' }}>
+          <input type="submit" value="Update" />
+          <input
+            style={{ marginLeft: '12px' }}
+            className="muted-button"
+            type="button"
+            value="Cancel"
+            onClick={() => setIsEditing(false)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Edit;
---
+++ src/components/Logout/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Swal from 'sweetalert2';
+
+const Logout = ({ setIsAuthenticated }) => {
+  const handleLogout = () => {
+    Swal.fire({
+      //set up the confirmation prompt
+      icon: 'question',
+      title: 'Logging Out',
+      text: 'Are you sure you want to log out?',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+    }).then(result => {
+      if (result.value) {
+        Swal.fire({
+          timer: 1500,
+          showConfirmButton: false,
+          willOpen: () => {
+            //display loading animation
+            Swal.showLoading();
+          },
+          willClose: () => {
+            //set authentication to false aka logged out
+            localStorage.setItem('is_authenticated', false);
+            setIsAuthenticated(false);
+          },
+        });
+      }
+    });
+  };
+
+  return (
+    <button
+      style={{ marginLeft: '12px' }}
+      className="muted-button"
+      onClick={handleLogout}
+    >
+      Logout
+    </button>
+  );
+};
+
+export default Logout;
